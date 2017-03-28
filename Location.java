72dbe6187c6c2014dd88caa5394847ed95d1eb29
rbearden <rbeard13@students.kennesaw.edu>
2017-03-27 21:29:47 -0400

    Update Location.java

--- Location.java
+++ Location.java
@@ -3,26 +3,43 @@
  *  Project:		Adventure Game Creation Commands
  */
 
-
-// Test Change
-
-
 package commands;
 
 public class Location
 {	// ----------------------------------- Data Fields
-	Object location;
+	private Object location; //can either be a Room or a Container
+	private boolean room, container;
 	
-	public Location(Map map, Room room)
-	{	this.location = room;
+	// ----------------------------------------------------- Constructor
+	public Location(Room location)
+	{	this.location = location;
+		room = true;
+		container = false;
 	}
 	
-	public Location(Map map, Container container)
-	{	this.location = container;
+	public Location(Container location)
+	{	this.location = location;
+		room = false;
+		container = true;
 	}
 	
-	public Object getLocation()
-	{	return this.location;		
+	// Check whether location is of type Room
+	public boolean isRoom()
+	{	return this.room;
 	}
+	
+	public Room getRoom()
+	{	return (Room)location;
+	}
+	
+	// Check whether location is of type Container
+	public boolean isContainer()
+	{	return this.container;
+	}
+	
+	public Container getContainer()
+	{	return (Container)location;		
+	}
+	
 } // end Location
 
