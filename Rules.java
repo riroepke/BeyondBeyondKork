41be3b9c0177508f499e24fa32e76ca1b6cf41e4
rbearden <rbeard13@students.kennesaw.edu>
2017-03-29 18:03:44 -0400

    Update Rules.java

--- Rules.java
+++ Rules.java
@@ -35,79 +35,216 @@ public class Rules implements Definitions
 		Item LAMP = new Item("lamp", "lantern", REPOSITORY, 10, "A brass lantern");
 		Item SWORD = new Item("sword", REPOSITORY, 10, "An elvish sword");
 		
+	// ========================================================== Reuasable Warning Messages for Player
+		private final String DEADEND = "You can't go that way";
+		private final String SARCASTIC_CHALLENGE = "How do you expect to accomplish that?";
+		
 	// ---------------------------------------------------- Constructor
-	public Rules(Map map)
+	public Rules()
 	{	
 	}
 	
 	// ----------------------------- Directional movement methods
 	// Generic movement method
-	public boolean travel(Person p, Room toRoom)
+	public boolean travel(Person p, Room destination)
 	{	boolean canTravel = true;
 		
-		if(toRoom != null)
-		{	Location toLocation = new Location(toRoom);
+		/*  null signifies dead end. If indicated destination is not a dead end,
+		 *   set person's location to the new location
+		 */
+		if(destination != null)
+		{	// Convert Room to more general location (either a Room or a Container)
+			Location toLocation = new Location(destination);
+			
+			// Set person's location to the new location
 			p.setLocation(toLocation);
 		}
-		else
+		else // The indicated destination is a dead end
 			canTravel = false;
 	
 		return canTravel;
 	}
 	
-	public String n(Person p)
+	/*	Each of the subsequent methods will attempt to move a person in a particular direction.
+	 * 	Each will
+	 * 	(1) Check person's current location
+	 * 		case 1: Person is in a Room; Move to (2)
+	 * 		case 2: Person is in a Container; Send warning message
+	 * 	(2) Check whether intended direction is a valid path
+	 * 		case 1: yes; return room description + items in room as a String
+	 * 		case 2: no; send a warning message
+	 */
+	
+	// (Documentation only included in n() method)
+	public String n(Person p) // --------------- Move person North
 	{	String message = null;
-		if(travel(p, p.getLocation().getRoom().getN()) == true)
-		{	message = p.getLocation().getRoom().getDescription();
-			
+		Room room;
+		
+		/****************** (1) Check person's current location *********************/
+		/* case 1: Person is in a Room*/
+		if(p.getLocation().isRoom())
+		{	room = p.getLocation().getRoom(); // Convert person's location to a Room
+		
+			/****************(2) Cehck whether intended direction is a valid path ***/
+			/* case 1: yes */
+			if(travel(p, room.getN()) == true)
+			{	message = room.getDescription();
+				message += room.getItemDescriptions();
+			}
+			/* case 2: no */
+			else
+				message = DEADEND;
 		}
 		
+		/* case 2: Person is in a Container*/
+		else
+			message = SARCASTIC_CHALLENGE;
 		
 		return message;
 	}
-	public String ne(Person p)
+	public String ne(Person p)// --------------- Move person Northeast
 	{	String message = null;
+		Room room;
+		if(p.getLocation().isRoom())
+		{	room = p.getLocation().getRoom();
+			if(travel(p, room.getNE()) == true)
+			{	message = room.getDescription();
+				message += room.getItemDescriptions();
+			}
+			else
+				message = DEADEND;
+		}
+		else
+			message = SARCASTIC_CHALLENGE;
+		return message;
+	}
+	public String e(Person p)// --------------- Move person East
+	{	String message = null;
+		Room room;
+		if(p.getLocation().isRoom())
+		{	room = p.getLocation().getRoom();
+			if(travel(p, room.getE()) == true)
+			{	message = room.getDescription();
+				message += room.getItemDescriptions();
+			}
+			else
+				message = DEADEND;
+		}
+		else
+			message = SARCASTIC_CHALLENGE;
 		
 		return message;
 	}
-	public String e(Person p)
+	public String se(Person p)// --------------- Move person Southeast
 	{	String message = null;
-			
-		return message;
-	}
-	public String se(Person p)
-	{	String message = null;
-		
+		Room room;
+		if(p.getLocation().isRoom())
+		{	room = p.getLocation().getRoom();
+			if(travel(p, room.getSE()) == true)
+			{	message = room.getDescription();
+				message += room.getItemDescriptions();
+			}
+			else
+				message = DEADEND;
+		}
+		else
+			message = SARCASTIC_CHALLENGE;
 		return message;	
 	}
-	public String s(Person p)
+	public String s(Person p)// --------------- Move person South
 	{	String message = null;
-		
+		Room room;
+		if(p.getLocation().isRoom())
+		{	room = p.getLocation().getRoom();
+			if(travel(p, room.getS()) == true)
+			{	message = room.getDescription();
+				message += room.getItemDescriptions();
+			}
+			else
+				message = DEADEND;
+		}
+		else
+			message = SARCASTIC_CHALLENGE;
 		return message;
 	}
-	public String sw(Person p)
+	public String sw(Person p)// --------------- Move person Southwest
 	{	String message = null;
-		
+		Room room;
+		if(p.getLocation().isRoom())
+		{	room = p.getLocation().getRoom();
+			if(travel(p, room.getSW()) == true)
+			{	message = room.getDescription();
+				message += room.getItemDescriptions();
+			}
+			else
+				message = DEADEND;
+		}
+		else
+			message = SARCASTIC_CHALLENGE;
 		return message;
 	}
-	public String w(Person p)
+	public String w(Person p)// --------------- Move person West
 	{	String message = null;
-		
+		Room room;
+		if(p.getLocation().isRoom())
+		{	room = p.getLocation().getRoom();
+			if(travel(p, room.getW()) == true)
+			{	message = room.getDescription();
+				message += room.getItemDescriptions();
+			}
+			else
+				message = DEADEND;
+		}
+		else
+			message = SARCASTIC_CHALLENGE;
 		return message;		
 	}
-	public String nw(Person p)
+	public String nw(Person p)// --------------- Move person Northwest
 	{	String message = null;
-		
+		Room room;
+		if(p.getLocation().isRoom())
+		{	room = p.getLocation().getRoom();
+			if(travel(p, room.getNW()) == true)
+			{	message = room.getDescription();
+				message += room.getItemDescriptions();
+			}
+			else
+				message = DEADEND;
+		}
+		else
+			message = SARCASTIC_CHALLENGE;
 		return message;		
 	}
-	public String u(Person p)
+	public String u(Person p)// --------------- Move person Up
 	{	String message = null;
-		
+		Room room;
+		if(p.getLocation().isRoom())
+		{	room = p.getLocation().getRoom();
+			if(travel(p, room.getU()) == true)
+			{	message = room.getDescription();
+				message += room.getItemDescriptions();
+			}
+			else
+				message = DEADEND;
+		}
+		else
+			message = SARCASTIC_CHALLENGE;
 		return message;		
 	}
-	public String d(Person p)
+	public String d(Person p)// --------------- Move person Down
 	{	String message = null;
-		
+		Room room;
+		if(p.getLocation().isRoom())
+		{	room = p.getLocation().getRoom();
+			if(travel(p, room.getD()) == true)
+			{	message = room.getDescription();
+				message += room.getItemDescriptions();
+			}
+			else
+				message = DEADEND;
+		}
+		else
+			message = SARCASTIC_CHALLENGE;
 		return message;		
 	}
 	
