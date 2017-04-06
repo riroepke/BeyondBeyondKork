d9c3157f0d69a93d1d50253aed3649ccf9597a0f
rbearden <rbeard13@students.kennesaw.edu>
2017-04-06 14:33:58 -0400

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -3,34 +3,31 @@ package commands;
 import java.util.Scanner;
 
 public class Parser
-{	// --------- Data Fields
-	private String[] args;
-	private java.util.Scanner input = new Scanner(System.in); 
+{	private String[] args;                                    // List of words in input line
+	private java.util.Scanner input = new Scanner(System.in); // Scanner object for getting user input
 
-	// --------- Constructors
+	// -------------------------------------------------------------------------------- Constructors
 	public Parser() {}
 	
-	// --------- Methods
-	public String[] getLine()
-	{	this.args = this.input.nextLine().split(" ");
+	// ------------------------------------------------------------------------------------- Methods
+	// Read in a single line of input
+	public String[] getLine() 
+	{	this.args = this.input.nextLine().split(" "); // Obtain input as a series of indexed Strings
 		
-		if(this.args.length > 0)
-			for(int i = 0; i < args.length; i++)
-				this.args[i] = this.args[i].trim();
+		if(this.args.length > 0)                      // Make sure input is not empty
+			for(int i = 0; i < args.length; i++)      
+				this.args[i] = this.args[i].trim();   // Trim each word
 		
 		return this.args;
 	}
 	
-	public String[] getPreviousLine()
-	{	return this.args;
-	}
-	
+	// Get word by index
 	public String getWord(int i)
 	{	String s = null;
 		
 		if(args.length >= i+1)
-			s = this.args[i];
+			s = this.args[i]; // Return word at index i
 		
 		return s;
 	}
-}
+} // end Parser
