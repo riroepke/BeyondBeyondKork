1b5d85f138898a25c489a240e6aac22522716c6e
riroepke <rebekah.roepke@icloud.com>
2017-04-06 16:09:29 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,8 +1,41 @@
-package commands;
+package Commands;
 
-public class Game
+public class Game extends ItemList
 {	public static void main(String[] args)
-	{	Parser input = new Parser();
+	{	
+	//creation of the map
+	Room CaveEntrance = new Room("Cave Entrance","It appears to be a large rock blocking the entrance to a cave. "
+			+ "Right next to the entrance there is a picket sign with some writing on it", "the Cave's entrance");//the sign will have the welcome message and basic rules
+	//set items in room(picket sign)
+	CaveEntrance.addItem(picketSign);
+	CaveEntrance.addItem(rock);//will be able to move but not taken, event handler once its moved, you can enter cavern
+	
+	Map.addRoom(CaveEntrance.getName());//add to map
+	
+	Room InsideCave = new Room("Inside Cave","You're now inside the cave. It's quite dark but you can make"
+	+"out a faint light coming from a portable lantern, a bag of dog treats and a half empty canteen","a dark, dank, cave room");
+	//set items in room(lamp, dog treats, canteen)
+	InsideCave.addItem(lantern);
+	InsideCave.addItem(dogTreat);
+	InsideCave.addItem(canteen);
+	
+	Map.addRoom(InsideCave.getName());//add to map
+	
+	Room MirrorRoom = new Room("Mirror Room","You just entered the Mirror Room. It looks like someone made this part of the cave into a living room."
+			+"You see a hungry dog in the corner and a mirror hanging on the wall.You get this ominous feeling that someone is watching you through the mirror.",
+			"looks like a room with a mirror in it");//make room and add descriptions
+	//set the items in the room(dog, sword and mirror)
+	MirrorRoom.addItem(dog);//once fed the treats it will reveal the sword thta will be used to break the mirror
+	MirrorRoom.addItem(sword);//will be used to break mirror
+	MirrorRoom.addItem(mirror);//the item will be set as breakable an once broken the condition is met so the hidden path to the treasure room is open
+	
+	Map.addRoom(MirrorRoom.getName());//adds room to map
+	
+	Room TreasureRoom = new Room("Treasure Room","You've made it to the treassure room! Congradulations! Grab all the treasure you can carry","Looks like a room filled with many treassures");
+	
+	
+	
+		Parser input = new Parser();
 		String action;
 		
 		boolean keepPlaying = true;
@@ -15,7 +48,8 @@ public class Game
 			// Check for direction input
 			
 		}
-	
+		
+		
 	} // end main()
 
 	// ----------------------- Define movement
