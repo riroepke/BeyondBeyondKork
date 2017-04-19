8707c34925fc63be70ca611ff087079679991c05
rbearden <rbeard13@students.kennesaw.edu>
2017-04-18 22:32:28 -0400

    Update Rules.java

--- Rules.java
+++ Rules.java
@@ -1,74 +1,75 @@
-package Commands;
+package commands;
 
 import java.util.Scanner;
 
 public class Rules 
 {	// ---------------------------------------------------- Data Fields
+	
 	// =========================================================== Define 1 room in which to "store" unused items
-		static Location REPOSITORY = new Location(new Room(""));
+		protected static Location REPOSITORY = new Location(new Room(""));
 	
 	// =========================================================== Action Definitions
 		// Format:  Action ACT = new Action(inputID, takesObject, takesObjectWithPreposition) 
 		
 		// Directions
-		protected Action N = new Action("north", "n", false, false);
-		protected Action NE = new Action("northeast", "ne", false, false);
-		protected Action E = new Action("east", "e", false, false);
-		protected Action SE = new Action("southeast", "se", false, false);
-		protected Action S = new Action("south", "s", false, false);
-		protected Action SW = new Action("southwest", "sw", false, false);
-		protected Action W = new Action("west", "w", false, false);
-		protected Action NW = new Action("northwest", "nw", false, false);
-		protected Action U = new Action("up", "u", false, false);
-		protected Action D = new Action("down", "d", false, false);
+		protected static Action N = new Action("north", "n", false, false);
+		protected static Action NE = new Action("northeast", "ne", false, false);
+		protected static Action E = new Action("east", "e", false, false);
+		protected static Action SE = new Action("southeast", "se", false, false);
+		protected static Action S = new Action("south", "s", false, false);
+		protected static Action SW = new Action("southwest", "sw", false, false);
+		protected static Action W = new Action("west", "w", false, false);
+		protected static Action NW = new Action("northwest", "nw", false, false);
+		protected static Action U = new Action("up", "u", false, false);
+		protected static Action D = new Action("down", "d", false, false);
 		
 		// Other actions
-		protected Action LOOK_IN_DIRECTION = new Action("look", "l", true, false);
-		protected Action LOOK = new Action("look", "l", false, false);	
-		protected Action TAKE = new Action("take", true, false);
-		protected Action MOVE = new Action("move", true, false);
-		protected Action SWING = new Action("swing", true, true);
-		protected Action JUMP = new Action("jump", false, false);
-		protected Action LIGHT = new Action("light", true, true);
-		protected Action TURN_ON = new Action("turn on", true, false);  // SPECIAL CASE
+		protected static Action LOOK_IN_DIRECTION = new Action("look", "l", true, false);
+		protected static Action LOOK_IN = new Action("look in", true, false);
+		protected static Action LOOK = new Action("look", "l", false, false);	
+		protected static Action TAKE = new Action("take", true, false);
+		protected static Action DROP = new Action("drop", true, false);
+		protected static Action MOVE = new Action("move", true, false);
+		protected static Action SWING = new Action("swing", true, true);
+		protected static Action JUMP = new Action("jump", false, false);
+		protected static Action LIGHT = new Action("light", true, true);
+		protected static Action READ = new Action("read", true, false);
+		protected static Action EXAMINE = new Action("examine", true, false);
+		protected static Action DRINK = new Action("drink", true, false);
+		protected static Action EAT = new Action("eat", true, false);
+		protected static Action FEED = new Action("feed", true, true);
+		protected static Action BREAK = new Action("break", true, true);
+		protected static Action TURN_ON = new Action("turn on", true, false);  // SPECIAL CASE
+		protected static Action INVENTORY = new Action("inventory", "i", false, false);
 		
 	// ========================================================== Object Definitions
 
-		 protected static Item PICKETSIGN = new Item("Picket Sign","Sign",REPOSITORY,0,"Welcome to the mysterious caverns of Kork."
-	+"Beyond this heavy rock is a maze filled with unnimaginable treasures and formidible fows.");
-		//add actions(can be read, cant be taken,can look)
-		
-		protected static Item ROCK = new Item("Rock", "Big rock",REPOSITORY,20,"the rock looks pretty heavy, but you think you can move it");
-		//add actions(can be moved,cant be taken, can look)
-		
-		protected static Item LANTERN = new Item("Lantern","Lamp",REPOSITORY,5,"battery powered, portable lantern. Can be used to ligth dark rooms");
-		//add actions(can light, can be taken, can look)
-		
-		protected static Item CANTEEN = new Item("Canteen","Bottle",REPOSITORY,2,"canteen that is half filled with water.Could be usefull later.");
-		//add actions(can drink,can be taken, can look)
-		
-		protected static Item DOGTREAT = new Item("Dog treat","treat",REPOSITORY,1,"small bag of dog treats");
-		//add actions(can be taken, can eat, can look)
-		
-		protected static Item DOG = new Item("Dog","puppy",REPOSITORY,30,"hungry looking dog. It seems like it is gaurding something");
-		//add actions(can be fed, cant be taken, can look)
-		
-		protected static Item SWORD = new Item("Sword","magic sword",REPOSITORY,10,"an ancient looking sword. Looks like you could do a bunch of damage with this item.Possibly break a couple things as well.");
-		//add actions(can swing, can look)
-		
-		protected static Item MIRROR = new Item("Mirror","2-way mirror",REPOSITORY,10,"something abouot this mirror is a little off. It seems like someone can see  through the other side."
+		protected static Item PICKETSIGN = new Item("sign","picket",REPOSITORY,0,"Welcome to the mysterious caverns of Kork."
+				+"Beyond this heavy rock is a maze filled with unnimaginable treasures and formidible fows.");
+		protected static Item ROCK = new Item("rock", "boulder",REPOSITORY,2000,"the rock looks pretty heavy, but you think you can move it");
+		protected static Item LANTERN = new Item("lantern","lamp",REPOSITORY,5,"battery powered, portable lantern. Can be used to ligth dark rooms");
+		protected static Item CANTEEN = new Item("canteen","bottle",REPOSITORY,2,"canteen that is half filled with water.Could be usefull later.");
+		protected static Item DOGTREAT = new Item("treat", "lump" ,REPOSITORY,1,"small bag of dog treats");
+		protected static Item DOG = new Item("dog","puppy",REPOSITORY,30,"hungry looking dog. It seems like it is gaurding something");
+		protected static Item SWORD = new Item("sword",REPOSITORY,10,"an ancient looking sword. Looks like you could do a bunch of damage with this item.Possibly break a couple things as well.");
+		protected static Item MIRROR = new Item("mirror",REPOSITORY,10,"something abouot this mirror is a little off. It seems like someone can see  through the other side."
 				+"You also feel a slight draft coming from the wall its hanging on");
-		//add actions(can break, can look
 		
-	// ========================================================== Reuasable Warning Messages for Player
+	// ========================================================== Reusable Warning Messages for Player
 		private final String DEADEND = "You can't go that way";
 		private final String SARCASTIC_CHALLENGE = "How do you expect to accomplish that?";
 		
 	// ---------------------------------------------------- Constructor
 	public Rules()
 	{	// Initialize actions for Item variables
+		PICKETSIGN.addActions(READ, EXAMINE);
+		ROCK.addActions(MOVE, EXAMINE, TAKE);
 		LANTERN.addActions(TAKE, LIGHT, TURN_ON, SWING, MOVE);
-		SWORD.addActions(TAKE, SWING, MOVE);
+		CANTEEN.addActions(DRINK, LOOK_IN, EXAMINE);
+		DOGTREAT.addActions(TAKE, EAT, EXAMINE);
+		DOG.addActions(FEED);
+		SWORD.addActions(TAKE, SWING, MOVE, EXAMINE);
+		MIRROR.addActions(BREAK, LOOK_IN);
 	}
 	
 	
@@ -108,4 +109,4 @@ public class Rules
 		return message;		
 	}
 
-}  // end Rules
+} // end Rules
