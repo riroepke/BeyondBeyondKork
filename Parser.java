b8b33b92bc1f0f105e6b82accfeeefcf2f83dedc
rbearden <rbeard13@students.kennesaw.edu>
2017-04-18 22:31:29 -0400

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -2,22 +2,24 @@ package commands;
 
 import java.util.Scanner;
 
-public class Parser
+public class Parser extends Rules
 {	private String[] args;                                    // List of words in input line
-	private java.util.Scanner input = new Scanner(System.in); // Scanner object for getting user input
+	private String inputString;                                      // Scanner object for getting user input
 
 	// -------------------------------------------------------------------------------- Constructors
-	public Parser() {}
+	public Parser(String inputString)
+	{	this.inputString = inputString;		
+	}
 	
 	// ------------------------------------------------------------------------------------- Methods
 	// Read in a single line of input
 	public String[] getLine() 
-	{	this.args = this.input.nextLine().split(" "); // Obtain input as a series of indexed Strings
+	{	this.args = this.inputString.split(" "); // Obtain input as a series of indexed Strings
 		
 		if(this.args.length > 0)                      // Make sure input is not empty
-			for(int i = 0; i < args.length; i++)      
-				this.args[i] = this.args[i].trim();   // Trim each word
-		
+			for(int i = 0; i < args.length; i++)      // Make sure each word is stored in standard form
+				this.args[i] = this.args[i].trim().toLowerCase();   
+			
 		return this.args;
 	}
 	
@@ -30,4 +32,10 @@ public class Parser
 		
 		return s;
 	}
+	
+	// Get the number of words in args
+	public int numberOfWords()
+	{	return this.args.length;		
+	}
+	
 } // end Parser
