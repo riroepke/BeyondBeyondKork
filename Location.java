f736c5b062b2ab2f272eba2a046ea4271ae7c244
rbearden <rbeard13@students.kennesaw.edu>
2017-04-18 22:30:40 -0400

    Update Location.java

--- Location.java
+++ Location.java
@@ -32,6 +32,13 @@ public class Location
 	{	return (Room)location;
 	}
 	
+	public String getName()
+	{	if(isRoom())
+			return getRoom().getName();
+		else
+			return getContainer().getName();		
+	}
+	
 	// Check whether location is of type Container
 	public boolean isContainer()
 	{	return this.container;
@@ -64,14 +71,72 @@ public class Location
 		return message;
 	}
 	
+	public String getBearings()
+	{	String message = null;
+		if(this.isRoom())
+			message = this.getRoom().getExitList();
+		else
+			message = this.getContainer().getInsideDescription();
+		return message;
+	}
+	
+	public String getListedItems()
+	{	String itemDescriptions = null;
+		if(this.isRoom())
+			itemDescriptions = this.getRoom().getListedItems();
+		else
+			itemDescriptions = this.getContainer().getListedItems();
+		
+		return itemDescriptions;
+	}
+	
 	public String getItemDescriptions()
 	{	String itemDescriptions = null;
 		if(this.isRoom())
 			itemDescriptions = this.getRoom().getItemDescriptions();
 		else
 			itemDescriptions = this.getContainer().getItemDescriptions();
-		
+
 		return itemDescriptions;
+	}
+	
+	
+	// Generic method for finding item in a location (null if it doesn't exist)
+	public Item findItem(String itemName)
+	{	Item item = null;
+	
+		// make sure itemName is in standard form
+		itemName = itemName.trim().toLowerCase();
+		
+		if(this.isRoom())
+			item = getRoom().getItemFromRoom(itemName);
+		else
+			item = getContainer().getItemFromContainer(itemName);
+		
+		return item;
+	}
+	
+	// ============================== Generic methods for adding and removing items
+	public boolean addItem(Item newItem)
+	{	boolean itemAdded = false;
+	
+		if(this.isRoom())
+			itemAdded = getRoom().addItem(newItem);
+		else
+			itemAdded = getContainer().addItem(newItem);
+		
+		return itemAdded;
+	}
+	
+	public boolean removeItem(Item item)
+	{	boolean itemRemoved = false;
+	
+		if(this.isRoom())
+			itemRemoved = getRoom().removeItem(item);
+		else
+			itemRemoved = getContainer().addItem(item);
+		
+		return itemRemoved;
 	}
 	
 } // end Location
